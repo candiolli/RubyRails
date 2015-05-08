db30a46f01d51bf655baff06e64a166e1eafdc4e
candiolli <silasca@gmail.com>
2015-05-08 16:31:35 -0300

    commit
    commit

---
+++ Glossario/teste.rb
@@ -0,0 +1,10 @@
+print "Integer please: "
+user_num = 1
+
+if user_num < 0
+  puts "menor que 0"
+elsif user_num > 0
+  puts "maior que 0"
+else
+  puts "You picked zero!"
+end
---
+++ Glossario/teste_arrays.rb
@@ -0,0 +1,14 @@
+matzes = Array.new(3, "Matz!")
+puts matzes
+
+bobbies = ["Bobby!", "Bobby!", "Backend Bobby!"]
+#puts bobbies
+
+bobbies.each do |arg|
+	puts arg
+end
+
+[1,2,3,4].each do |number|
+  puts "You know what number I love?"
+  puts "I love #{number}!"
+end
